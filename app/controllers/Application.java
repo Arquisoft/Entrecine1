b338464e0fd05231d0d835f6fdef3861add3f8c1
DiegoBarrantes <uo219317@EII-L02A-09.aulaseinginformatica.uniovi.es>
2013-05-02 09:42:05 +0200

    Cambios

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import models.Movie;
 import models.Session;
 import persistence.PersistenceFactory;
 import persistence.PersistenceFactoryImpl;
